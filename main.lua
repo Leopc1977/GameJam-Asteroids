9fdbbc76c2257c8aa02e55ecd55d19c81f2ecc05
Leopc1977 <leophancaowz@gmail.com>
2019-11-19 19:43:23 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,67 +8,71 @@ if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 function love.load ()
   
---load
+-- récupère height/width fullscreen
+  desktopWidth, desktopHeight = love.window.getDesktopDimensions( display )
+
+--LOAD
   love.window.setTitle( "Asteroids" )
   love.window.setFullscreen(true)
   
   backgroundSpace = {}
   --load table background
 
+  -- background
+  backgroundSpace.background = {}
+
   --particule
   backgroundSpace.particule = {}
-  backgroundSpace.particule.x = {}
-  backgroundSpace.particule.y = {}
 
   --saturne
   backgroundSpace.saturne = {}
-  backgroundSpace.saturne.x = {}
-  backgroundSpace.saturne.y = {}
 
   --twinPlanet
   backgroundSpace.twinPlanet = {}
-  backgroundSpace.twinPlanet.x = {}
-  backgroundSpace.twinPlanet.y = {}
 
   --bigPlanet
   backgroundSpace.bigPlanet = {}
-  backgroundSpace.bigPlanet.x = {}
-  backgroundSpace.bigPlanet.y = {}
---load background
-  backgroundSpace.img = love.graphics.newImage("images/background/parallax-space-backgound.png")
-  backgroundSpace.particule = love.graphics.newImage("images/background/parallax-space-stars.png")
-  backgroundSpace.saturne = love.graphics.newImage("images/background/parallax-space-ring-planet.png")
-  backgroundSpace.twinPlanet = love.graphics.newImage("images/background/parallax-space-far-planets.png")
-  backgroundSpace.bigPlanet = love.graphics.newImage("images/background/parallax-space-big-planet.png")
 
--- récupère height/width fullscreen
-  deskopWidth, deskopHeight = love.window.getDesktopDimensions( display )
--- récupère height/width de parallax-space-backgound.png
-backgroundSpace.height = backgroundSpace.img:getHeight()
-backgroundSpace.width = backgroundSpace.img:getWidth()
+
+--load img backgroundS
+  backgroundSpace.background.img = love.graphics.newImage("images/background/parallax-space-backgound.png")
+  backgroundSpace.particule.img = love.graphics.newImage("images/background/parallax-space-stars.png")
+  backgroundSpace.saturne.img  = love.graphics.newImage("images/background/parallax-space-ring-planet.png")
+  backgroundSpace.twinPlanet.img  = love.graphics.newImage("images/background/parallax-space-far-planets.png")
+  backgroundSpace.bigPlanet.img  = love.graphics.newImage("images/background/parallax-space-big-planet.png")
+
+-- récupere --paramètres-- de backgroundSpace.particule
+ 	 backgroundSpace.particule.x = desktopWidth/2
+  	backgroundSpace.particule.y = desktopHeight/2
+	backgroundSpace.particule.height = backgroundSpace.particule.img:getHeight()
+	backgroundSpace.particule.width = backgroundSpace.particule.img:getWidth()
+
+-- recupere --paramètres-- de saturne
+
+	backgroundSpace.saturne.x = desktopWidth/2
+	backgroundSpace.saturne.y = desktopHeight/2
+	backgroundSpace.saturne.height = backgroundSpace.saturne.img:getHeight()
+	backgroundSpace.saturne.width = backgroundSpace.saturne.img:getWidth()
+
+-- recupere --paramètres-- de twinPlanet
+
+	backgroundSpace.twinPlanet.x = desktopWidth/2
+	backgroundSpace.twinPlanet.y = desktopHeight/2
+	backgroundSpace.twinPlanet.height = backgroundSpace.twinPlanet.img:getHeight()
+	backgroundSpace.twinPlanet.width = backgroundSpace.twinPlanet.img:getWidth()
+
+-- recupere --paramètres-- de bigPlanet
+
+	--backgroundSpace.bigPlanet.y =
+	--backgroundSpace.bigPlanet.y =
+
+-- récupère --paramètres-- de parallax-space-backgound
+	backgroundSpace.background.height = backgroundSpace.background.img:getHeight()
+	backgroundSpace.background.width = backgroundSpace.background.img:getWidth()
 
 -- convertit image -> fullscreen
-backgroundSpace.scaleX = deskopWidth / backgroundSpace.width
-backgroundSpace.scaleY = deskopWidth / backgroundSpace.height
-
--- load x et y de backgroundSpace.particule
-  backgroundSpace.particule.x = 0
-  backgroundSpace.particule.y = 0
-
--- load x et y de saturne
-
-	--backgroundSpace.saturne.x =
-	--backgroundSpace.saturne.y =
-
--- load --paramètres-- de twinPlanet
-
-	backgroundSpace.twinPlanet.x = deskopWidth/2
-	backgroundSpace.twinPlanet.y = deskopHeight/2
-
--- load x et y de bigPlanet
-
-	--backgroundSpace.bigPlanet.y =
-	--backgroundSpace.bigPlanet.y =
+	backgroundSpace.scaleX = desktopWidth / backgroundSpace.background.width
+	backgroundSpace.scaleY = desktopWidth / backgroundSpace.background.height
 
 end
 
@@ -81,9 +85,13 @@ end
 function love.draw ()
 
 --Background inGame
-  love.graphics.draw(backgroundSpace.img, deskopWidth/2,deskopHeight,0,backgroundSpace.scaleX,backgroundSpace.scaleY,backgroundSpace.width/2,backgroundSpace.height)
+
+  love.graphics.draw(backgroundSpace.background.img, desktopWidth/2,desktopHeight,0,backgroundSpace.scaleX,backgroundSpace.scaleY,backgroundSpace.background.width/2,backgroundSpace.background.height)
   --particule
-  love.graphics.draw(backgroundSpace.particule,backgroundSpace.particule.x,backgroundSpace.particule.y,0,backgroundSpace.scaleX / 2,backgroundSpace.scaleY/2,backgroundSpace.width/2,backgroundSpace.height)
+  love.graphics.draw(backgroundSpace.particule.img ,backgroundSpace.particule.x,backgroundSpace.particule.y,0,backgroundSpace.scaleX /4,backgroundSpace.scaleY/4,backgroundSpace.particule.width/2,backgroundSpace.particule.height)
   --twinPlanet
-  love.graphics.draw(backgroundSpace.twinPlanet, backgroundSpace.twinPlanet.x, backgroundSpace.twinPlanet.y,0,backgroundSpace.scaleX / 2,backgroundSpace.scaleY/2)
+  love.graphics.draw(backgroundSpace.twinPlanet.img , backgroundSpace.twinPlanet.x, backgroundSpace.twinPlanet.y,0,backgroundSpace.scaleX,backgroundSpace.scaleY/2, backgroundSpace.twinPlanet.width/2, backgroundSpace.twinPlanet.height/2)
+  --saturne
+  love.graphics.draw(backgroundSpace.saturne.img, backgroundSpace.saturne.x, backgroundSpace.twinPlanet.y,0, backgroundSpace.scaleX/3, backgroundSpace.scaleY/3, backgroundSpace.saturne.width/2, backgroundSpace.saturne.height/2)
+
 end	
