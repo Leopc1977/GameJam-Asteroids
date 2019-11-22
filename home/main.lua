bc89b4fc0751684a7874b4cdc74c4e5ee718439f
Leopc1977 <leophancaowz@gmail.com>
2019-11-22 20:07:19 +0100

    Update main.lua

--- home/main.lua
+++ home/main.lua
@@ -1,3 +1,6 @@
+-- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no')
+
 function love.load()
 
 	love.window.setFullscreen(true)
@@ -42,7 +45,7 @@ function love.update(dt)
 end
 
 function love.draw()
-	
+
 	--title
 	love.graphics.rectangle("fill",desktopWidth/2 - widthT/2, yT, widthT, heightT)
 
@@ -51,23 +54,28 @@ function love.draw()
   	love.graphics.print(xM,1,1)
   	love.graphics.print(yM,1,20)
 
-	if xM > x[1] and xM < x[1] + width and yM > y[1] and yM < y[1] + height then
+	function love.mousepressed (xM, yM, button)
+
+	if xM > x[1] and xM < x[1] + width and yM > y[1] and yM < y[1] + height and button == 1 then
 		love.graphics.print("good 1 !", 1,30)
+		print("1")
 	end
 
-	if xM > x[1] and xM < x[1] + width and yM > y[2] and yM < y[2] + height then
+	if xM > x[1] and xM < x[1] + width and yM > y[2] and yM < y[2] + height and button == 1 then
 		love.graphics.print("good 2 !", 1,40)
+		print("2")
 	end
 
-	if xM > x[1] and xM < x[1] + width and yM > y[3] and yM < y[3] + height then
+	if xM > x[1] and xM < x[1] + width and yM > y[3] and yM < y[3] + height and button == 1 then
 		love.graphics.print("good 3 !", 1,50)
+		print("3")
 	end
 
-	if xM > x[1] and xM < x[1] + width and yM > y[4] and yM < y[4] + height then
+	if xM > x[1] and xM < x[1] + width and yM > y[4] and yM < y[4] + height and button == 1 then
 		love.graphics.print("good 4 !", 1,60)
+		print("4")
 	end
-
-
+end
   	--rectangle
   	love.graphics.rectangle("fill",x[1],y[1], width, height)
 	love.graphics.rectangle("fill",x[1],y[2], width, height)
