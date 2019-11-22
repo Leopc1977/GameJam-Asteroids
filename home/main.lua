ea842fb44c13b8f179abc6cebb45a6e9594781f8
Leopc1977 <leophancaowz@gmail.com>
2019-11-22 18:55:18 +0100

    Update main.lua

--- home/main.lua
+++ home/main.lua
@@ -19,8 +19,8 @@ function love.load()
 
 	y[1] = desktopHeight/2 - height/1
 	y[2] = y[1] + height + gap
-	y[3] = y[1] + height + gap
-	y[4] = y[2] + height + gap
+	y[3] = y[1] + height + gap + height + gap
+	y[4] = y[2] + height + gap + height + gap
 
 	x[1] = desktopWidth/2 - width/2
 
@@ -46,13 +46,32 @@ function love.draw()
 	--title
 	love.graphics.rectangle("fill",desktopWidth/2 - widthT/2, yT, widthT, heightT)
 
+	--test
   	xM, yM = love.mouse.getPosition()
   	love.graphics.print(xM,1,1)
   	love.graphics.print(yM,1,20)
+
+	if xM > x[1] and xM < x[1] + width and yM > y[1] and yM < y[1] + height then
+		love.graphics.print("good 1 !", 1,30)
+	end
+
+	if xM > x[1] and xM < x[1] + width and yM > y[2] and yM < y[2] + height then
+		love.graphics.print("good 2 !", 1,40)
+	end
+
+	if xM > x[1] and xM < x[1] + width and yM > y[3] and yM < y[3] + height then
+		love.graphics.print("good 3 !", 1,50)
+	end
+
+	if xM > x[1] and xM < x[1] + width and yM > y[4] and yM < y[4] + height then
+		love.graphics.print("good 4 !", 1,60)
+	end
+
+
   	--rectangle
   	love.graphics.rectangle("fill",x[1],y[1], width, height)
 	love.graphics.rectangle("fill",x[1],y[2], width, height)
-	love.graphics.rectangle("fill",x[1],y[3] + height + gap, width, height)
-	love.graphics.rectangle("fill",x[1],y[4] + height + gap, width, height)
+	love.graphics.rectangle("fill",x[1],y[3], width, height)
+	love.graphics.rectangle("fill",x[1],y[4], width, height)
 
 end
