53af187ee30a20d61e22ea5637724bb9f78456e5
Leopc1977 <leophancaowz@gmail.com>
2019-11-23 12:01:16 +0100

    Update main.lua

--- option/main.lua
+++ option/main.lua
@@ -17,25 +17,30 @@ function love.load()
 	--parametres name 
 	option.name.width = desktopWidth * 22.5 / 100
 	option.name.height = desktopHeight * 12.5 / 100
-	-- name.y
+	option.name.nameX = desktopWidth/2 - option.name.width/2
+	--parametres bar
+	option.bar = {}
+	option.bar.width = 60*desktopWidth/100
+	option.bar.height = 10*desktopHeight/100
+	option.bar.x =  desktopWidth/2 - option.bar.width/2
+
+	-- name.y et bar.x
 	--TITLE
 	option.name[1] = {}
 	option.name[1].y = 100
 	--option
 	option.name[2] = {}
-	option.name[2].y = 300
+	option.name[2].y = 350
 
 	option.name[3] = {}
 	option.name[3].y = 	option.name[2].y + option.name.height + gap[1]
-	option.name[3].width = 60*desktopWidth/100
-	option.name[3].height = 10*desktopHeight/100
 
 	option.name[4] = {}
-	--option.name[4].y = option.name[3].y + option.name.height + gap[1]
+	option.name[4].y = option.name[3].y + option.name.height + gap[2]
 
-	--x pour les rectangle, toujours au milieu de l'ecran
-	option.name.nameX = desktopWidth/2 - option.name.width/2
-	option.name.barX = desktopWidth/2 - option.name[3].width/2
+	option.name[5] = {}
+	option.name[5].y = option.name[4].y + option.name.height + gap[1]
+
 
 end
 
@@ -51,8 +56,9 @@ function love.draw()
 	love.graphics.rectangle("fill",	option.name.nameX, option.name[1].y, option.name.width, option.name.height)
 	--option
 	love.graphics.rectangle("fill",	option.name.nameX, option.name[2].y, option.name.width, option.name.height)
-	love.graphics.rectangle("fill",	option.name.barX, option.name[3].y, option.name[3].width, option.name[3].height)
+	love.graphics.rectangle("fill",	option.bar.x, option.name[3].y, option.bar.width, option.bar.height)
 
-	--love.graphics.rectangle("fill",	option.name.nameX, option.name[4].y, option.name.width, option.name.height)
+	love.graphics.rectangle("fill",	option.name.nameX, option.name[4].y, option.name.width, option.name.height)
+	love.graphics.rectangle("fill",	option.bar.x, option.name[5].y, option.bar.width, option.bar.height)
 
 end
